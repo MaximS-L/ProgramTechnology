94488a95197a7ad2fed1f53a5060cb4dc9adc272
MaximS-L <msluzivenko@gmail.com>
2026-09-25 13:04:05 +0700

    #1 Созданы классы BankAccount  и Transaction

---
+++ bank/bank/Transaction.cs
@@ -0,0 +1,19 @@
+﻿
+namespace bank;
+// мы создали благодаря record неизменяемый тип данных
+internal record Transaction(decimal Amount, DateTime Date, string Note);
+
+//internal record Transaction
+//{
+//    public decimal Amount { get; }
+//    public decimal Date { get; }
+//    public decimal Note { get; }
+//    public Transaction(decimal Amount, decimal Date, decimal Note)
+//    {
+//        this.Note = Note;
+//        this.Amount = Amount;
+//        this.Date = Date;
+//    }
+
+//}
+
